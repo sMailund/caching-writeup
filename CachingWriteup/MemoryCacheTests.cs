3c779132835ab01fec4831ee36635c006c2417c4
Simen Mailund Svendsen <simen@erus.no>
2024-11-01 13:14:58 +0100

    add comments

--- CachingWriteup/MemoryCacheTests.cs
+++ CachingWriteup/MemoryCacheTests.cs
@@ -9,17 +9,24 @@ public class MemoryCacheTests
     {
         var cacheKey = Guid.NewGuid().ToString();
         
-        var myCache = new MemoryCache(new MemoryCacheOptions());
-        myCache.Set(cacheKey, new TestObject()
+        // siden vi bare jobber i lokalt minne lager vi cache med IMemoryCache
+        IMemoryCache myCache = new MemoryCache(new MemoryCacheOptions());
+        
+        var testObject = new TestObject()
         {
             Value = 1
-        });
+        };
+        
+        myCache.Set(cacheKey, testObject);
 
+        // her henter vi ut objektet vi satte inn 2 ganger, de deler referanse til samme objekt i minne
         var copy1 = myCache.Get<TestObject>(cacheKey);
         var copy2 = myCache.Get<TestObject>(cacheKey);
 
+        // så når vi endrer via den ene pekeren...
         copy1.Value = 1337;
 
+        // ...har vi endra på objektet som den andre pekeren også peker på
         Assert.Equal(copy1.Value, copy2.Value);
     }
 }
